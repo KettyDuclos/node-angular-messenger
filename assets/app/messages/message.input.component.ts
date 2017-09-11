b260bc57efd6931c3460c072df9c3e6d819a38a4
KettyDuclos <ketty.duclos11@gmail.com>
2017-09-10 22:51:32 -0400

    created message service and form validation

--- assets/app/messages/message.input.component.ts
+++ assets/app/messages/message.input.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+import {MessageService} from './message.service';
+import {Message} from './message.model';
+
 
 @Component({
     selector: 'app-message-input',
@@ -6,7 +11,12 @@ import {Component} from '@angular/core';
 })
 
 export class MessageInputComponent{
-    onSave(value: string){
-        console.log(value);
+    constructor(private messageService: MessageService){}
+
+    onSubmit(form: NgForm){
+        console.log(form);
+        const message = new Message(form.value.content, 'Ketty');
+        this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
--- assets/app/messages/message.list.component.ts
+++ assets/app/messages/message.list.component.ts
@@ -1,22 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Message} from './message.model';
+import {MessageService} from './message.service';
 
 @Component({
     selector: 'app-message-list',
     template: `
         <div class="col-md-8 col-md-offset-2">
-        <app-message [message]="message"
-        (editClicked)="message.content = $event"
-        *ngFor="let message of messages"></app-message> 
+            <app-message [message]="message"
+            (editClicked)="message.content = $event"
+            *ngFor="let message of messages"></app-message> 
         </div>
     `
 })
 
 
-export class MessageListComponent{
-    messages: Message[] = [
-        new Message('Some Message 1','Ketty'),
-        new Message('Some Message 2','Ketty'),
-        new Message('Some Message 3','Ketty')
-    ]
+export class MessageListComponent implements OnInit{
+    messages: Message[];
+
+    constructor(private messageService: MessageService){}
+
+    ngOnInit() {
+        this.messages = this.messageService.getMessages();
+    }
 }
