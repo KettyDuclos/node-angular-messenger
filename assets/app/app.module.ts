3292742eb580bbb48a150ff6d8d05452a9066f11
KettyDuclos <ketty.duclos11@gmail.com>
2017-09-18 18:50:53 -0400

    completed backend and frontend signup feature

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -14,6 +14,7 @@ import {routing} from './app.routing';
 import {LogoutComponent} from './auth/logout.component';
 import {SigninComponent} from './auth/signin.component';
 import {SignupComponent} from './auth/signup.component';
+import {AuthService} from './auth/auth.service';
 
 
 @NgModule({
@@ -37,6 +38,7 @@ import {SignupComponent} from './auth/signup.component';
         ReactiveFormsModule,
         HttpModule
     ],
+    providers: [AuthService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
