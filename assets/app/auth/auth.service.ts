79af2742bc177478187dece5453f5c768cdd8f80
KettyDuclos <ketty.duclos11@gmail.com>
2017-09-18 21:26:00 -0400

    completed frontend and backend signin functionality

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -8,6 +8,7 @@ import { User } from "./user.model";
 @Injectable()
 export class AuthService{
     constructor(private http: Http) {}
+
     signup(user: User) {
         const body = JSON.stringify(user);
         const headers = new Headers({'Content-Type': 'application/json'});
@@ -15,4 +16,14 @@ export class AuthService{
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
+
+    signin(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3005/user/signin', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+
+    
 }
